68e61c7d26cab9a4f847af272dabc21f347a7638
ko9un <ktw1633@gmail.com>
2022-09-20 17:19:12 +0900

    9.20 공부

---
+++ sec02/src/sec02_primarydatatype/EX04_TypeCasting_1/TypeCasting_1.java
@@ -0,0 +1,10 @@
+package sec02_primarydatatype.EX04_TypeCasting_1;
+
+public class TypeCasting_1 {
+	
+	public static void main(String[] args) {
+		System.out.println((double)3/2);
+		System.out.println((double)(3/2));
+	}
+
+}
